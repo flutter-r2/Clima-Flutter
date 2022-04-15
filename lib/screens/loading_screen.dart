32753843fca5fc7217bf6f6f0a6b319a1e0972ad
Artur <arturbr@gmail.com>
2022-04-15 17:43:18 -0300

    Getting weather from current location

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -4,25 +4,38 @@ import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+const apiKey = 'b6907d289e10d714a6e88b30761fae22'; // only for 'sample' server
+//only works for a fixed lat/lon pair, change to 'api' to use unrestrictedly
+const server = 'sample';
+
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  Location location = Location();
+  double lat;
+  double lon;
 
   @override
   void initState() {
     super.initState();
+    getLocation();
+  }
 
-    location.currentLocation();
+  Future<void> getLocation() async {
+    Location location = Location();
+    await location.currentLocation();
+
+    lat = location.lat;
+    lat = location.lon;
+
     getWeatherData();
   }
 
   Future<void> getWeatherData() async {
     http.Response response = await http.get(Uri.parse(
-        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22'));
+        'https://$server.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=$apiKey'));
 
     if (response.statusCode == 200) {
       String data = response.body;
@@ -46,7 +59,6 @@ class _LoadingScreenState extends State<LoadingScreen> {
       body: Center(
         child: ElevatedButton(
           onPressed: () {
-            location.currentLocation();
             getWeatherData();
           },
           child: Text('Get Location'),
